c781ff96e747961a89728430aa3ee2899f75eb9f
Fernando <fernandomunozpuente@gmail.com>
2024-06-26 23:58:56 +0200

    cleaned main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import bronch-skele as sk
 
-PATH_STL_16 = ""
-PATH_STL_FULL = ""
+PATH_STL = ""
 
-PATH_TERMINALS_TO_KEEP_16 = ""
-PATH_TERMINALS_TO_KEEP_FULL = ""
+PATH_TERMINALS_TO_KEEP = ""
 
-OUTPUT_FOLDER_16 = "./lung_16"
-OUTPUT_FOLDER_FULL = "./lung_full"
+OUTPUT_FOLDER = "./lung_full"
 
 POS_TRACHEA = (0.45, 13.4, -1.4)
 POS_PROPAGATE_LOBES = {"RS": [(-4.43, 9.38, -12.69)],
@@ -22,22 +19,13 @@ PATH_LOBES = ""
 PATH_LUNG_RIGHT = ""
 PATH_LUNG_LEFT = ""
 
-# ---------- SKELETON EXTRACTION FOR BOTH MESHES -----------
-
-# LUNG_16, arrives to 17th generation
-sk.stl2skel(PATH_STL_16, POS_TRACHEA, PATH_TERMINALS_TO_KEEP_16, OUTPUT_FOLDER_16)
+# ---------- SKELETON EXTRACTION, INTRABRONCHIAL DISTANCE MATRIX AND TERMINAL INFORMATION -----------
+sk.stl2skel(PATH_STL, POS_TRACHEA, PATH_TERMINALS_TO_KEEP, OUTPUT_FOLDER)
 print("NOW EXTRACTING DATA...")
-sk.extract_data(OUTPUT_FOLDER_16, POS_TRACHEA, POS_PROPAGATE_LOBES)
-sk.intrabronchial_distance(OUTPUT_FOLDER_16 + "/aux/graph_2.graphml", POS_TRACHEA, OUTPUT_FOLDER_16 + "/D.txt")
-sk.extract_terminals_for_bm(OUTPUT_FOLDER_16 + "/aux/graph_2.graphml", POS_TRACHEA, PATH_LOBULES, PATH_LOBES,
-                             PATH_LUNG_RIGHT, PATH_LUNG_LEFT, OUTPUT_FOLDER_16 + "/terminals.txt")
-
-# LUNG_FULL, arrives to 23rd generation
-sk.stl2skel(PATH_STL_FULL, POS_TRACHEA, PATH_TERMINALS_TO_KEEP_FULL, OUTPUT_FOLDER_FULL)
-print("NOW EXTRACTING DATA...")
-sk.extract_data(OUTPUT_FOLDER_FULL, POS_TRACHEA, POS_PROPAGATE_LOBES)
-sk.intrabronchial_distance(OUTPUT_FOLDER_FULL + "/aux/graph_2.graphml", POS_TRACHEA, OUTPUT_FOLDER_FULL + "/D.txt")
-sk.extract_terminals_for_bm(OUTPUT_FOLDER_FULL + "/aux/graph_2.graphml", POS_TRACHEA, PATH_LOBULES, PATH_LOBES,
-                             PATH_LUNG_RIGHT, PATH_LUNG_LEFT, OUTPUT_FOLDER_FULL + "/terminals.txt")
-
+sk.extract_data(OUTPUT_FOLDER, POS_TRACHEA, POS_PROPAGATE_LOBES)
+print("INTRABRONCHIAL DISTANCE...")
+sk.intrabronchial_distance(OUTPUT_FOLDER + "/aux/graph_2.graphml", POS_TRACHEA, OUTPUT_FOLDER + "/D.txt")
+print("TERMINALS FOR BUBBLE MODEL...")
+sk.extract_terminals_for_bm(OUTPUT_FOLDER + "/aux/graph_2.graphml", POS_TRACHEA, PATH_LOBULES, PATH_LOBES,
+                             PATH_LUNG_RIGHT, PATH_LUNG_LEFT, OUTPUT_FOLDER + "/terminals.txt")
 
